2f27d65ceecd0a3470e3032c1a5a5482a202baf0
simonetripodi <simone.tripodi@gmail.com>
2012-07-12 14:12:34 +0200

    initial checkin of BaseTraverseHandler class

---
+++ core/src/main/java/com/terradue/warhol/traverse/BaseTraverseHandler.java
@@ -0,0 +1,40 @@
+package com.terradue.warhol.traverse;
+
+/*
+ *    Copyright 2011-2012 Terradue srl
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import org.w3._2005.atom.Entry;
+
+/**
+ * Base {@link TraverseHandler} implementation which methods do nothing.
+ */
+public abstract class BaseTraverseHandler
+    implements TraverseHandler
+{
+
+    @Override
+    public void onSeries( Entry series )
+    {
+        // do nothing
+    }
+
+    @Override
+    public void onProduct( Entry product )
+    {
+        // do nothing
+    }
+
+}
