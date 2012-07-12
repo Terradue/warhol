96eee536f1fb585040c8ed13779c2411ebc00e41
simonetripodi <simone.tripodi@gmail.com>
2012-07-12 17:26:22 +0200

    store locally the Catalogue refernce

--- core/src/main/java/com/terradue/warhol/CatalogueConnector.java
+++ core/src/main/java/com/terradue/warhol/CatalogueConnector.java
@@ -16,21 +16,44 @@ package com.terradue.warhol;
  *    limitations under the License.
  */
 
+import static com.terradue.warhol.lang.Preconditions.checkNotNullArgument;
+
 import org.w3._2005.atom.Feed;
 
 import com.sun.jersey.api.client.Client;
 import com.terradue.warhol.settings.Catalogue;
+import com.terradue.warhol.traverse.CatalogueTraverseHandler;
+import com.terradue.warhol.traverse.CatalogueTraverseHandlerBuilder;
 
 public final class CatalogueConnector
 {
 
     private static final String ATOM_XML = "application/atom+xml";
 
+    private final Catalogue catalogue;
+
     private final Client restClient;
 
     public CatalogueConnector( Catalogue catalogue )
     {
+        this.catalogue = checkNotNullArgument( catalogue, "Impossible to initialize a CatalogueConnector froma  null Catalogue" );
         restClient = null;
+    }
+
+    public CatalogueTraverseHandlerBuilder traverse()
+    {
+        return new CatalogueTraverseHandlerBuilder()
+        {
+
+            @Override
+            public void with( CatalogueTraverseHandler traverseHandler )
+            {
+                traverseHandler = checkNotNullArgument( traverseHandler, "Impossible to traverse the Catalogue <%s> with a null handler" );
+
+
+            }
+
+        };
     }
 
     Feed parse( String uri )
