78c8828c554fd5f37f9b1c5b048f2cc4f2a79d9c
simonetripodi <simone.tripodi@gmail.com>
2012-07-12 16:04:21 +0200

    LocalStorageConnector initialization

--- core/src/main/java/com/terradue/warhol/LocalStorageConnector.java
+++ core/src/main/java/com/terradue/warhol/LocalStorageConnector.java
@@ -16,14 +16,28 @@ package com.terradue.warhol;
  *    limitations under the License.
  */
 
+import static com.terradue.warhol.lang.Preconditions.checkArgument;
+import static com.terradue.warhol.lang.Preconditions.checkState;
+
+import java.io.File;
+
 import com.terradue.warhol.settings.LocalStorage;
 
 public final class LocalStorageConnector
 {
 
+    private final File baseDir;
+
     public LocalStorageConnector( LocalStorage localStorage )
     {
-        // TODO
+        checkArgument( localStorage != null, "Impossible to create a new LocalStorage connector from a null LocalStorage" );
+        baseDir = new File( localStorage.getBaseDir() );
+        checkState( baseDir.exists(),
+                    "Impossible to create a new LocalStorage connector, base local storage directory %s does not exist",
+                    baseDir );
+        checkState( baseDir.isDirectory(),
+                    "Impossible to create a new LocalStorage connector, base local storage %s is not a file",
+                    baseDir );
     }
 
 }
