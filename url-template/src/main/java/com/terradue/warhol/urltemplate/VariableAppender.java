9af59028d09d3eaf47efce8054a5994dee93317a
simonetripodi <simone.tripodi@gmail.com>
2012-07-13 09:50:54 +0200

    initial checkin of appenders for text fragments

---
+++ url-template/src/main/java/com/terradue/warhol/urltemplate/VariableAppender.java
@@ -0,0 +1,50 @@
+package com.terradue.warhol.urltemplate;
+
+/*
+ *    Copyright 2011-2012 Terradue srl
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import static java.lang.String.format;
+
+import java.util.Map;
+
+final class VariableAppender
+    implements Appender
+{
+
+    private final String variableName;
+
+    public VariableAppender( String variableName )
+    {
+        this.variableName = variableName;
+    }
+
+    @Override
+    public void append( StringBuilder buffer, Map<String, String> variables )
+    {
+        String variableValue = variables.get( variableName );
+        if ( variableValue != null )
+        {
+            buffer.append( variableValue );
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        return format( "{%s}" );
+    }
+
+}
