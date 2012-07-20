61439c1ac25d9feb6686633896e7fae465479524
simonetripodi <simone.tripodi@gmail.com>
2012-07-20 17:14:33 +0200

    better execution time format

--- client/cli/src/main/java/com/terradue/warhol/client/cli/Warhol.java
+++ client/cli/src/main/java/com/terradue/warhol/client/cli/Warhol.java
@@ -28,6 +28,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.File;
 import java.util.Date;
+import java.util.Formatter;
 import java.util.Map;
 
 import javax.xml.bind.JAXBContext;
@@ -198,7 +199,32 @@ public final class Warhol
                 logger.info( "" );
             }
 
-            logger.info( "Total time: {}s", ( ( currentTimeMillis() - start ) / 1000 ) );
+            Formatter uptime = new Formatter().format( "Total uptime:" );
+
+            long uptimeInSeconds = ( currentTimeMillis() - start ) / 1000;
+            final long hours = uptimeInSeconds / 3600;
+
+            if ( hours > 0 )
+            {
+                uptime.format( " %s hour%s", hours, ( hours > 1 ? "s" : "" ) );
+            }
+
+            uptimeInSeconds = uptimeInSeconds - ( hours * 3600 );
+            final long minutes = uptimeInSeconds / 60;
+
+            if ( minutes > 0 )
+            {
+                uptime.format( " %s minute%s", minutes, ( minutes > 1 ? "s" : "" ) );
+            }
+
+            uptimeInSeconds = uptimeInSeconds - ( minutes * 60 );
+
+            if ( uptimeInSeconds > 0 )
+            {
+                uptime.format( " %s second%s", uptimeInSeconds, ( uptimeInSeconds > 1 ? "s" : "" ) );
+            }
+
+            logger.info( "Total time: {}", uptime.toString() );
             logger.info( "Finished at: {}", new Date() );
 
             final Runtime runtime = getRuntime();
